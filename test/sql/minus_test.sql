0009d23c888925ccb69d81e9bc19701f071e6e8b
Marco Martinez <m-martinez@users.noreply.github.com>
2017-09-12 16:51:23 -0700

    Reorganize tests to match use case

--- test/sql/minus_test.sql
+++ test/sql/minus_test.sql
@@ -1,12 +1,25 @@
 CREATE EXTENSION "pg-audit-json";
+
 -- Remove one key
 SELECT '{"a": 1, "b": 2, "c": 3}'::jsonb - '{a}'::text[];
+
 -- Remove multiple keys
 SELECT '{"a": 1, "b": 2, "c": 3}'::jsonb - '{a,c}'::text[];
--- Return deletions from the original document
-SELECT '{"a": 1, "b": 2, "c": 3}'::jsonb - '{"a": 1}'::jsonb;
--- Return changes from the original document
-SELECT '{"a": 1, "b": 2, "c": 3}'::jsonb - '{"a": 1, "b": 99, "c": 3}'::jsonb;
--- Return changes from the original document (recursive)
-SELECT '{"a":1, "b":{"c":123, "d":"test"}}'::jsonb
-    - '{"a":1, "b":{"c":321, "d":"test"}}'::jsonb;
+
+-- Test usage scenario where NEW - OLD
+
+-- Removed c
+SELECT '{"a": 1, "b": 2}'::jsonb
+     - '{"a": 1, "b": 2, "c": 3}'::jsonb;
+
+-- Changed b
+SELECT '{"a": 1, "b": 99, "c": 3}'::jsonb
+     - '{"a": 1, "b": 2, "c": 3}'::jsonb;
+
+-- Added d
+SELECT '{"a": 1, "b": 2, "c": 3, "d": 4}'::jsonb
+     - '{"a": 1, "b": 2, "c": 3}'::jsonb;
+
+-- Recursive changed b->c
+SELECT '{"a":1, "b":{"c":321, "d":"test"}}'::jsonb
+     - '{"a":1, "b":{"c":123, "d":"test"}}'::jsonb;
