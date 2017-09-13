6c538c77ad5dd638d9821d058b9d92df458e63a7
Marco Martinez <m-martinez@users.noreply.github.com>
2017-09-12 17:48:17 -0700

    Cleanup after test file

--- test/sql/minus_test.sql
+++ test/sql/minus_test.sql
@@ -23,3 +23,5 @@ SELECT '{"a": 1, "b": 2, "c": 3, "d": 4}'::jsonb
 -- Recursive changed b->c
 SELECT '{"a":1, "b":{"c":321, "d":"test"}}'::jsonb
      - '{"a":1, "b":{"c":123, "d":"test"}}'::jsonb;
+
+DROP EXTENSION "pg-audit-json";
